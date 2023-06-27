693466e24522b6b2e39ff45d4c1041d7bc1d18c9
Permlap <permlapteam@gmail.com>
2023-06-27 23:38:17 +0700

    first feedback :happy:

--- src/student.jsx
+++ src/student.jsx
@@ -1,84 +1,73 @@
-import studentList from "./studentList"
-import './student.css'
+import studentList from "./studentList";
+import "./student.css";
 import groupStudents from "./studentgroup";
-import React, { useState, useEffect } from 'react';
-
-
+import React, { useState, useEffect } from "react";
 
 function Students(props) {
-    let avatar = 'https://i.pravatar.cc/150?img=';
-    
-    const studentNum = props.num;
-    const numOfGroup = props.numOfStdGroup;
-    const studentClassList = studentList.slice(0, studentNum);
-    const randStudentList = studentClassList.sort(() => Math.random() - 0.5);
-    const stdGroup = groupStudents(randStudentList,numOfGroup)
+  // never use let to declear variable inside react function it will lead to the future bug, instead please use useState.
+  let avatar = "https://i.pravatar.cc/150?img=";
 
-    const [showList, setShowList] = useState(studentClassList);
-    const [isGroup, setIsGroup] = useState(false);
-    const toggleGroup = () => {
-      setIsGroup(value => !value);
-      console.log("Toggle");
-      console.log(isGroup);
-    };
-    
-    
-    const handleGroup = (group) => {
-      //for (let i = 0; i < stdGroup.length; i++) {
-      //   console.log("Group " + (i + 1) + ": " + stdGroup[i]);
-      // }
+  const studentNum = props.num;
+  const numOfGroup = props.numOfStdGroup;
+  const studentClassList = studentList.slice(0, studentNum);
+  const randStudentList = studentClassList.sort(() => Math.random() - 0.5);
+  const [showList, setShowList] = useState(studentClassList);
+  const [isGroup, setIsGroup] = useState(false);
+  // when calling fucntion should be declear argument clearly so you don't mess up later
+  // instead of groupStudents(randStudentList,numOfGroup) should do as below (it's the best practice for calling function)
+  const stdGroup = groupStudents({
+    students: randStudentList,
+    numGroups: numOfGroup,
+  });
 
-       for (let i = 0; i < group.length; i++) {
-         console.log("Group " + (i + 1) + ": " + group[i]);
-         
-       }
-    };
-    console.log(handleGroup(stdGroup));
-    
-    
-    
-    
-    return (
-      <>
-        <div className="student-title">
-          <h1>Student</h1>
-          <p> {studentClassList.length} in the classroom</p>
-          <p> num in props {studentNum}</p>
-          <button onClick={() => toggleGroup()}> {isGroup ? 'ungroup' : 'group'}</button>
-        </div>
-        <div className="student-card">
-           {isGroup ? 
-            stdGroup.map((subArray, index) => (
+  const toggleGroup = () => {
+    setIsGroup((value) => !value);
+    console.log("Toggle");
+    console.log(isGroup);
+  };
+
+  // any function that not be called should comment it out
+  // const handleGroup = (group) => {
+  //   for (let i = 0; i < group.length; i++) {
+  //     console.log("Group " + (i + 1) + ": " + group[i]);
+  //   }
+  // };
+
+  return (
+    <>
+      <div className="student-title">
+        <h1>Student</h1>
+        <p> {studentClassList.length} in the classroom</p>
+        <p> num in props {studentNum}</p>
+        <button onClick={() => toggleGroup()}>
+          {" "}
+          {isGroup ? "ungroup" : "group"}
+        </button>
+      </div>
+      <div className="student-card">
+        {isGroup
+          ? stdGroup.map((subArray, index) => (
               <div className="student-group" key={index}>
-                <p>Group{index+1}</p>
+                <p>Group{index + 1}</p>
                 {subArray.map((st) => (
-                <div className="student-item" key={st.id}>
-                <img src= {`${avatar}${st.id}`} className="student-avatar"/>
-               <p>id :{st.id}</p>
-               <p>name: {st.name}</p>
-                </div>
+                  <div className="student-item" key={st.id}>
+                    <img src={`${avatar}${st.id}`} className="student-avatar" />
+                    <p>id :{st.id}</p>
+                    <p>name: {st.name}</p>
+                  </div>
                 ))}
-                </div>
-                )):showList.map((st) => (
-               <div className="student-item" key={st.id}>
-               <img src= {`${avatar}${st.id}`} className="student-avatar"/>
-               <p>id :{st.id}</p>
-               <p>name: {st.name}</p>
-                </div>
-            
+              </div>
+            ))
+          : showList.map((st) => (
+              <div className="student-item" key={st.id}>
+                <img src={`${avatar}${st.id}`} className="student-avatar" />
+                <p>id :{st.id}</p>
+                <p>name: {st.name}</p>
+              </div>
             ))}
-       
+      </div>
+    </>
+  );
+}
 
-            
-              
-          
-        </div>
-        
-        
-        
-      </>
-    )
-  }
-  
-  export default Students
-  
+export default Students;
--- src/studentgroup.jsx
+++ src/studentgroup.jsx
@@ -1,18 +1,19 @@
-function groupStudents(students, numGroups) {
-    let numStudents = students.length;
-    
-    let groups = new Array(numGroups);
-  
-    for (let i = 0; i < numGroups; i++) {
-      groups[i] = [];
-    }
-  
-    for (let j = 0; j < numStudents; j++) {
-      let groupIndex = j % numGroups;
-      groups[groupIndex].push(students[j]);
-    }
-  
-    return groups;
+// for parameter this should be the best practice not just function groupStudents( students, numGroups )
+function groupStudents({ students, numGroups }) {
+  let numStudents = students.length;
+
+  let groups = new Array(numGroups);
+
+  for (let i = 0; i < numGroups; i++) {
+    groups[i] = [];
   }
 
-  export default groupStudents
+  for (let j = 0; j < numStudents; j++) {
+    let groupIndex = j % numGroups;
+    groups[groupIndex].push(students[j]);
+  }
+
+  return groups;
+}
+
+export default groupStudents;
